68f581122134426fae4af5e7e4d9ba8bada934b6
Patrick Reisert <kpreisert@gmail.com>
2015-05-25 18:08:47 +0200

    Fixed and extended currencies example

--- examples/currencies.rs
+++ examples/currencies.rs
@@ -1,4 +1,4 @@
-#![cfg_attr(feature="unstable", feature(unboxed_closures,core))]
+#![cfg_attr(feature="unstable", feature(unboxed_closures,core,zero_one))]
 
 #[macro_use]
 extern crate units;
@@ -12,7 +12,7 @@ mod currencies {
     }
 }
 
-use currencies::f64::{euro, dollar};
+use currencies::f64::{euro, dollar, Euro};
 
 #[cfg(not(feature = "unstable"))]
 fn main() {
@@ -20,9 +20,11 @@ fn main() {
     let dollar_per_euro = 1.1006*dollar/euro;
     
     let a = 2.0*euro;
-    
     let b = a*dollar_per_euro;
     println!("{:?} = {:?}", a, b);
+    
+    let euro2dollar = |x: Euro| x * dollar_per_euro;
+    println!("{:?} = {:?}", 4.0*euro, euro2dollar(4.0*euro));
     
     let c = 3.0*dollar;
     let d = c/dollar_per_euro;
@@ -32,10 +34,13 @@ fn main() {
 #[cfg(feature = "unstable")]
 fn main() {
     let dollar_per_euro = (dollar/euro)(1.1006);
-    let a = euro(2.0);
     
+    let a = euro(2.0);
     let b = a*dollar_per_euro;
     println!("{:?} = {:?}", a, b);
+    
+    let euro2dollar = |x: Euro| x * dollar_per_euro;
+    println!("{:?} = {:?}", euro(4.0), euro2dollar(euro(4.0)));
     
     let c = dollar(3.0);
     let d = c/dollar_per_euro;
