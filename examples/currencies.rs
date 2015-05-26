5fa04db859b19dad737ad2e6a57fd964c36b1536
Patrick Reisert <kpreisert@gmail.com>
2015-05-26 11:09:34 +0200

    Better unit names in currencies example

--- examples/currencies.rs
+++ examples/currencies.rs
@@ -6,44 +6,44 @@ extern crate units;
 mod currencies {
     units! {
         Currency {
-            Euro[euro],
-            Dollar[dollar]
+            Euro[eur],
+            USDollar[usd]
         }
     }
 }
 
 use currencies::{Currency, Euro};
-use currencies::f64::{euro, dollar};
+use currencies::f64::{eur, usd};
 
 #[cfg(not(feature = "unstable"))]
 fn main() {
     // the conversion factor could also be retrieved from a database
-    let dollar_per_euro = 1.1006*dollar/euro;
+    let usd_per_eur = 1.1006*usd/eur;
     
-    let a = 2.0*euro;
-    let b = a*dollar_per_euro;
+    let a = 2.0*eur;
+    let b = a*usd_per_eur;
     println!("{:?} = {:?}", a, b);
     
-    let euro2dollar = |x: Currency<Euro,_>| x * dollar_per_euro;
-    println!("{:?} = {:?}", 4.0*euro, euro2dollar(4.0*euro));
+    let eur2usd = |x: Currency<Euro,_>| x * usd_per_eur;
+    println!("{:?} = {:?}", 4.0*eur, eur2usd(4.0*eur));
     
-    let c = 3.0*dollar;
-    let d = c/dollar_per_euro;
+    let c = 3.0*usd;
+    let d = c/usd_per_eur;
     println!("{:?} = {:?}", c, d);
 }
 
 #[cfg(feature = "unstable")]
 fn main() {
-    let dollar_per_euro = (dollar/euro)(1.1006);
+    let usd_per_eur = (usd/eur)(1.1006);
     
-    let a = euro(2.0);
-    let b = a*dollar_per_euro;
+    let a = eur(2.0);
+    let b = a*usd_per_eur;
     println!("{:?} = {:?}", a, b);
     
-    let euro2dollar = |x: Currency<Euro,_>| x * dollar_per_euro;
-    println!("{:?} = {:?}", euro(4.0), euro2dollar(euro(4.0)));
+    let eur2usd = |x: Currency<Euro,_>| x * usd_per_eur;
+    println!("{:?} = {:?}", eur(4.0), eur2usd(eur(4.0)));
     
-    let c = dollar(3.0);
-    let d = c/dollar_per_euro;
+    let c = usd(3.0);
+    let d = c/usd_per_eur;
     println!("{:?} = {:?}", c, d);
 }
